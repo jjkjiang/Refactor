9b7c638c46b2400c86750693c603765b46d76b5e
jjian014 <jjian014@ucr.edu>
2016-04-01 00:30:45 +0000

    Completed all known bugs

--- main.cpp
+++ main.cpp
@@ -6,20 +6,24 @@ using namespace std;
 
 int main(int argc, char* argv[] ) {
     
+    // checks if the program is run with the correct input.
     if (argc != 2) {
         cout << "Invalid input - this program is run by ./refactor.exe nameoffilehere" << endl;
         return 0;
     }
     
+    // initalize streams
     ifstream inFS;
     ofstream outFS;
     inFS.open(argv[1]);
     
+    // error message for opening
     if (!inFS.is_open()) {
         cout << "There was an error loading your file or it does not exist." << endl;
         return 0;
     }
     
+    // creates a target file to
     string fileName = argv[1];
     fileName += "_refactored";
     outFS.open(fileName.c_str());
@@ -32,13 +36,15 @@ int main(int argc, char* argv[] ) {
     getline(cin, original);
     cout << endl;
     
+    // dissallows replacing nothing
     if (original == "") {
         cout << "No input detected.";
         return 0;
     }
     
+    // warning
     if (original.length() < 4) {
-        cout << "Refactoring something with such a low length could be dangerous, continue? (y/n)";
+        cout << "Refactoring something with such a low length could be dangerous, continue? (y/n) ";
         cin >> warningResponse;
         cout << endl;
         cin.ignore();
@@ -54,15 +60,22 @@ int main(int argc, char* argv[] ) {
     string currentLine;
     
     while (getline(inFS, currentLine)) {
+        // initalize index of current in case skipping is needed
         int index = 0;
+        // continues getting from the file until eof
         while (currentLine.find(original, index) != string::npos) {
-            string firstHalf;
-            string secondHalf;
+            string firstHalf = "";
+            string secondHalf = "";
             int highestIndex = currentLine.size() - 1;
-            // check if back is allowed too.
-            if (currentLine.find(original, index) != 0 && currentLine.find(original, index) + 1 + original.length() < currentLine.length()) {
+            
+            // multiple branches in case you wish to refactor "int" but not variables such as "int1", or if you are
+            // going to refactor "in" and do not want "int" to be modifed at all.
+            
+            if (currentLine.find(original, index) != 0 && currentLine.find(original, index) + original.size() < highestIndex) {
+                // goes into this loop if NOT the front or back of the line
+                
                 char peekFront = currentLine.at(currentLine.find(original, index) - 1);
-                char peekBack = currentLine.at(currentLine.find(original, index) + original.length() - 1);
+                char peekBack = currentLine.at(currentLine.find(original, index) + original.length());
                 
                 if ((peekFront > 47 && peekFront < 58) || (peekFront > 64 && peekFront < 91) || 
                 (peekFront > 96 && peekFront < 123) || (peekBack > 47 && peekBack < 58) ||
@@ -72,8 +85,10 @@ int main(int argc, char* argv[] ) {
                     firstHalf = currentLine.substr(0, currentLine.find(original, index));
                     secondHalf = currentLine.substr(currentLine.find(original, index) + original.length(), original.length() - (currentLine.find(original, index) + original.length()));
                     currentLine = firstHalf + refactor + secondHalf;
-                }
+                } 
             } else if (currentLine.find(original, index) != 0) {
+                
+                // goes into this loop if NOT the front
                 char peekFront = currentLine.at(currentLine.find(original, index) - 1);
                 
                 if ((peekFront > 47 && peekFront < 58) || (peekFront > 64 && peekFront < 91) || 
@@ -83,8 +98,10 @@ int main(int argc, char* argv[] ) {
                     firstHalf = currentLine.substr(0, currentLine.find(original, index));
                     currentLine = firstHalf + refactor + secondHalf;
                 }
-            } else if (currentLine.find(original, index) + 1 + original.length() > currentLine.length()) {
-                char peekBack = currentLine.at(currentLine.find(original, index) + original.length() - 1);
+            } else if (currentLine.find(original, index) + original.size() < highestIndex) {
+                
+                // goes into this loop if NOT the back
+                char peekBack = currentLine.at(currentLine.find(original, index) + original.length());
                 if ((peekBack > 47 && peekBack < 58) || (peekBack > 64 && peekBack < 91) || 
                 (peekBack > 96 && peekBack < 123)) {
                     index = currentLine.find(original, index) + 1;
@@ -93,12 +110,15 @@ int main(int argc, char* argv[] ) {
                     currentLine = firstHalf + refactor + secondHalf;
                 }
             } else {
-                firstHalf = currentLine.substr(0, currentLine.find(original, index));
-                secondHalf = currentLine.substr(currentLine.find(original, index) + original.length(), original.length() - (currentLine.find(original, index) + original.length()));
+                
+                // goes into this loop if NOT any
                 currentLine = firstHalf + refactor + secondHalf;
             }
         }
         
         outFS << currentLine << endl;
     }
+    
+    cout << "Successfully refactored file to " << fileName << endl;
+    return 0;
 }
